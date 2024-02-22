a96d790841a1c29e7427f936d709dee43716be0b
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-02-22 15:28:17 +0200

    Update UserProvider.tsx
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- app/contexts/UserProvider.tsx
+++ app/contexts/UserProvider.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import { Page, useNavigation } from '../hooks/useNavigation';
@@ -6,21 +6,12 @@ import { User, UserContext } from '../hooks/useUser';
 import { useLocalStorage } from '../utils/localStorage';
 import { clearData } from '../utils/processData';
 
-export const UserProvider = ({ children }: { children: ReactNode }) => {
+export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { setPage, setNeedRefresh } = useNavigation();
 
   const connecting = useRef(false);
   const [user, setUser] = useLocalStorage<User | undefined>('user', undefined);
-  const [isConnected, setIsConnected] = useState(false);
-
-  useEffect(() => {
-    const isValidUser = !!user?.id && !!user?.name && !!user?.address;
-    if (!isValidUser) setUser(undefined);
-
-    setPage(isValidUser ? Page.Portfolio : Page.Dashboard);
-    setIsConnected(isValidUser);
-    setNeedRefresh(true);
-  }, [user, setPage, setUser, setNeedRefresh]);
+  const [isConnected, setIsConnected] = useState(!!user);
 
   const disconnect = useCallback(() => {
     setUser(undefined);
@@ -32,31 +23,37 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
       if (!userName || userName.toLowerCase() === user?.name.toLowerCase() || connecting.current) return user;
 
       connecting.current = true;
-      // if (!result.ok) throw new Error('Error while fetching users');
+      try {
+        const result = await fetch('/api/database/getUsers');
+        if (!result.ok) throw new Error('Error while fetching users');
 
-      return await fetch('/api/database/getUsers')
-        .then(result => (result.ok ? result.json() : undefined))
-        .then((users: User[]) => {
-          if (!users) throw new Error('Error while fetching users');
+        const users: User[] = await result.json();
+        const newUser = users.find(u => u.name.toLowerCase() === userName.toLowerCase());
 
-          const newUser = users.find(u => u.name.toLowerCase() === userName.toLowerCase());
-          if (newUser) {
-            setUser(newUser);
-          } else {
-            console.error('No user found');
-            disconnect();
-          }
-          return newUser;
-        })
-        .catch(error => {
-          console.error(error);
+        if (newUser) {
+          setUser(newUser);
+          setIsConnected(true);
+        } else {
+          console.error('No user found');
           disconnect();
-          return undefined;
-        })
-        .finally(() => (connecting.current = false));
+        }
+
+        return newUser;
+      } catch (error) {
+        console.error(error);
+        disconnect();
+        return undefined;
+      } finally {
+        connecting.current = false;
+      }
     },
     [disconnect, user, setUser],
   );
+
+  useEffect(() => {
+    setPage(isConnected ? Page.Portfolio : Page.Dashboard);
+    setNeedRefresh(true);
+  }, [isConnected, setPage, setNeedRefresh]);
 
   return (
     <UserContext.Provider
