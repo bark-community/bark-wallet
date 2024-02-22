c4b505396ef71dca5f28f852668124bba86c8e58
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-02-22 14:02:07 +0200

    Update gainsBar.tsx
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- app/components/gainsBar.tsx
+++ app/components/gainsBar.tsx
@@ -24,27 +24,19 @@ export default function GainsBar({ values, loaded }: { values: GainsBarProps | u
   const overKillValue = 10000 / (profitRatio * 100 + 100);
 
   return (
-    <>
-      <style>
-        {`.tremor-MarkerBar-rangeBar {
-            background-color: ${isPositive ? '#22c55e' : '#ef4444'};
-          }
-        `}
-      </style>
-      <Flex className="mt-4 mb-1">
-        {invested || !loaded ? (
-          <Subtitle className={cls('truncate w-0 text-left sm:w-1/2', !loaded ? 'blur-sm' : 'animate-unblur')}>
-            {`${t.invested} : ${invested.toLocaleCurrency()}`}
-          </Subtitle>
-        ) : null}
-        {invested || !loaded ? (
-          <Subtitle className={!loaded ? 'blur-sm' : 'animate-unblur'}>
-            {`${t.gains} : ${profitValue.toLocaleCurrency()}${profitRatio ? ' (' + profitRatio.toRatio() + ')' : ''}`}
-          </Subtitle>
-        ) : null}
-      </Flex>
+    <Flex className="mt-4 mb-1">
+      {(invested || !loaded) && (
+        <Subtitle className={cls('truncate w-0 text-left sm:w-1/2', !loaded && 'blur-sm')}>
+          {`${t.invested} : ${invested.toLocaleCurrency()}`}
+        </Subtitle>
+      )}
+      {(invested || !loaded) && (
+        <Subtitle className={!loaded && 'blur-sm'}>
+          {`${t.gains} : ${profitValue.toLocaleCurrency()}${profitRatio ? ` (${profitRatio.toRatio()})` : ''}`}
+        </Subtitle>
+      )}
 
-      {profitRatio || !loaded ? (
+      {(profitRatio || !loaded) && (
         <MarkerBar
           title="Gains"
           color={isPositive ? 'green' : 'red'}
@@ -52,7 +44,7 @@ export default function GainsBar({ values, loaded }: { values: GainsBarProps | u
           minValue={isOverKill ? overKillValue : isPositive ? 0 : 100 - Math.abs(profitRatio) * 100}
           maxValue={isOverKill ? 100 : isPositive ? profitRatio * 100 : 100}
         />
-      ) : null}
-    </>
+      )}
+    </Flex>
   );
 }
