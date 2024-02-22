c518310899e8ee876b1b7627ab009a42fa8a7340
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-02-22 15:21:03 +0200

    Update useNavigation.ts
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- app/hooks/useNavigation.ts
+++ app/hooks/useNavigation.ts
@@ -1,22 +1,20 @@
 import { createContext, useContext } from 'react';
 
-export enum Page {
-  Portfolio = 'Portfolio',
-  Transactions = 'Transactions',
-  Users = 'Users',
-  Dashboard = 'Dashboard', // Should stay at the end
+export interface User {
+  id: number;
+  name: string;
+  address: string;
 }
 
-export interface NavigationContextState {
-  page: Page | undefined;
-  setPage: (page: Page) => void;
-  pages: Page[];
-  needRefresh: boolean;
-  setNeedRefresh: (needRefresh: boolean) => void;
+export interface UserContextState {
+  user: User | undefined;
+  connect: (userName: string) => Promise<User | undefined>;
+  disconnect: () => void;
+  isConnected: boolean;
 }
 
-export const NavigationContext = createContext<NavigationContextState>({} as NavigationContextState);
+export const UserContext = createContext<UserContextState>({} as UserContextState);
 
-export function useNavigation(): NavigationContextState {
-  return useContext(NavigationContext);
+export function useUser(): UserContextState {
+  return useContext(UserContext);
 }
