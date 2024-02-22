e1a6508c885783294e89d9bf7cf96aad8137738d
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-02-22 15:13:39 +0200

    Update mobile.ts
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- app/utils/mobile.ts
+++ app/utils/mobile.ts
@@ -1,7 +1,10 @@
 import { useWindowParam } from '../hooks/useWindowParam';
 
-// Detects if the device is a mobile
-export function isMobileDevice() {
+/**
+ * Detects if the device is a mobile.
+ * @returns {boolean} True if the device is a mobile, otherwise false.
+ */
+export function isMobileDevice(): boolean {
   return (
     typeof window !== 'undefined' &&
     window.isSecureContext &&
@@ -10,13 +13,21 @@ export function isMobileDevice() {
   );
 }
 
-// Detects if the screen size is a mobile size (does not update on screen resize - use useIsMobile instead)
-export function isMobileSize(maxWidth = 768) {
+/**
+ * Detects if the screen size is a mobile size.
+ * @param {number} maxWidth - The maximum width considered as a mobile size. Default is 768 pixels.
+ * @returns {boolean} True if the screen size is a mobile size, otherwise false.
+ */
+export function isMobileSize(maxWidth: number = 768): boolean {
   return typeof window === 'undefined' || window.screen.availWidth < maxWidth;
 }
 
-// Detects if the screen size is a mobile size (update on screen resize)
-export function useIsMobile(maxWidth = 768) {
+/**
+ * Hook to dynamically detect if the screen size is a mobile size.
+ * @param {number} maxWidth - The maximum width considered as a mobile size. Default is 768 pixels.
+ * @returns {boolean} True if the screen size is a mobile size, otherwise false.
+ */
+export function useIsMobile(maxWidth: number = 768): boolean {
   const { width } = useWindowParam();
   return width < maxWidth && width > 0;
 }
