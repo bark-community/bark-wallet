88acceb38cc7e86948c3109765567fa713d0ddb8
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-02-22 15:25:03 +0200

    Update PopupProvider.tsx
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- app/contexts/PopupProvider.tsx
+++ app/contexts/PopupProvider.tsx
@@ -1,6 +1,6 @@
-'use client';
+// 'use client';
 
-import { ReactNode, createContext, useContext, useState } from 'react';
+import React, { ReactNode, createContext, useContext, useState } from 'react';
 
 export interface PopupContextState {
   isPopupOpen: boolean;
@@ -10,22 +10,34 @@ export interface PopupContextState {
   popup: ReactNode;
 }
 
-export const PopupContext = createContext<PopupContextState>({} as PopupContextState);
+export const PopupContext = createContext<PopupContextState>({
+  isPopupOpen: false,
+  openPopup: () => {},
+  closePopup: () => {},
+  autoClosePopup: () => {},
+  popup: null,
+});
 
-export const PopupProvider = ({ children }: { children: ReactNode }) => {
+export const PopupProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [popup, setPopup] = useState<ReactNode>();
   const [shouldCloseManually, setShouldCloseManually] = useState<boolean>();
+
+  const openPopup = (newPopup: ReactNode, shouldCloseManually?: boolean) => {
+    setPopup(newPopup);
+    setShouldCloseManually(shouldCloseManually);
+  };
+
+  const closePopup = () => setPopup(undefined);
+
+  const autoClosePopup = () => !shouldCloseManually && setPopup(undefined);
 
   return (
     <PopupContext.Provider
       value={{
         isPopupOpen: !!popup,
-        openPopup: (popup, shouldCloseManually) => {
-          setPopup(popup);
-          setShouldCloseManually(shouldCloseManually);
-        },
-        closePopup: () => setPopup(undefined),
-        autoClosePopup: () => (!shouldCloseManually ? setPopup(undefined) : undefined),
+        openPopup,
+        closePopup,
+        autoClosePopup,
         popup,
       }}
     >
@@ -34,6 +46,4 @@ export const PopupProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export function usePopup(): PopupContextState {
-  return useContext(PopupContext);
-}
+export const usePopup = (): PopupContextState => useContext(PopupContext);
